0e08f7a28f42be84ed2790c4bca43b80ef3c9b79
Davide Ori <dori@uni-koeln.de>
2017-10-02 12:06:02 +0200

    added basic functionalities to the parser

--- theta.cpp
+++ theta.cpp
@@ -13,18 +13,29 @@ namespace po = boost::program_options;
 #include <iterator>
 using namespace std;
 
+po::options_description description("Allowed options");
+po::variables_map vm;
+
 int main(int argn, char* argv[]) {
 	cout<<"Hello worlds!"<<endl;
-
-	po::options_description description("Allowed options");
-/*	description.add_options()
+	description.add_options()
 		("help", "produce help message")
 		("lambda", po::value<double>(),"set lambda")
 	;
 
-	po::variables_map vm;
 	po::store(po::parse_command_line(argn, argv, description), vm);
 	po::notify(vm);
-*/
+
+	if (vm.count("help")) {
+		cout<<"This is the theta program, please type --options for list of available options"<<endl;
+	}
+
+	if (vm.count("lambda")) {
+		cout<<"Lambda has been set to "<<vm["lambda"].as<double>()<<endl;
+	} else {
+		cout<<"lambda has not been set"<<endl;
+	}
+	
+
 	return 0;
 }
